024d6ee18525e9285b82d04fd91fdb2b00a54da5
Xabier de Zuazo <xabier@onddo.com>
2013-05-25 14:04:12 +0200

    metadata: added centos and debian support

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,8 @@ description      'Installs/Configures postfix-dovecot'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+supports 'centos'
+supports 'debian'
 supports 'ubuntu'
 
 depends 'dovecot'
