3c5c520fcc98a0a61fa0b7e882b54d0e4eb39e40
Xabier de Zuazo <xabier@onddo.com>
2013-05-30 20:43:12 +0200

    supported versions better specified

--- metadata.rb
+++ metadata.rb
@@ -6,9 +6,9 @@ description      'Installs/Configures postfix-dovecot'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-supports 'centos'
-supports 'debian'
-supports 'ubuntu'
+supports 'centos', '>= 6.0'
+supports 'debian', '>= 7.0'
+supports 'ubuntu', '>= 12.04'
 
 depends 'dovecot'
 depends 'onddo-spamassassin'
