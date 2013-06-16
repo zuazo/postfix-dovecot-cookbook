88d623070e45e680bd9a26ac5e2c9d4ddd6f2b3e
Xabier de Zuazo <xabier@onddo.com>
2013-06-16 13:59:42 +0200

    version bumped to 0.1.1 WiP

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'team@onddo.com'
 license          'Apache 2.0'
 description      'Installs and configures a mail server using Postfix, Dovecot, PostfixAdmin and SpamAssassin.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1' # WiP
 
 supports 'centos', '>= 6.0'
 supports 'debian', '>= 7.0'
