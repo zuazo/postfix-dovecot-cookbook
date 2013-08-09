f5eceddae8042c75617fb1d0273ad515a9fd3692
Xabier de Zuazo <xabier@onddo.com>
2013-08-09 19:08:05 +0200

    release v0.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,12 @@
 
 This file is used to list changes made in each version of `postfix-dovecot`.
 
+## 0.2.0:
+
+* Added [Amazon SES](http://aws.amazon.com/ses/) support.
+ * Added SES tests.
+* Fixed *resolv.conf* inside chroot in CentOS.
+
 ## 0.1.0:
 
 * Initial release of `postfix-dovecot`
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'team@onddo.com'
 license          'Apache 2.0'
 description      'Installs and configures a mail server using Postfix, Dovecot, PostfixAdmin and SpamAssassin, including Amazon SES support.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1' # WiP
+version          '0.2.0'
 
 supports 'centos', '>= 6.0'
 supports 'debian', '>= 7.0'
