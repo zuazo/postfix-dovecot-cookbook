b02aeda42b345dd3f56e13810c0b81595f0a1157
Xabier de Zuazo <xabier@onddo.com>
2014-10-04 11:54:19 +0200

    Integration tests updated to work with Serverspec 2

--- test/integration/postgresql/serverspec/spec_helper.rb
+++ test/integration/postgresql/serverspec/spec_helper.rb
@@ -19,11 +19,5 @@
 
 require 'serverspec'
 
-include Serverspec::Helper::Exec
-include Serverspec::Helper::DetectOS
-
-RSpec.configure do |c|
-  c.before :all do
-    c.os = backend(Serverspec::Commands::Base).check_os
-  end
-end
+# Set backend type
+set :backend, :exec
