2af0b42484770bfda0232c266441cef9802772d7
Xabier de Zuazo <xabier@zuazo.org>
2015-08-22 13:46:32 +0200

    Minitest: remove some redundant tests

--- test/cookbooks/postfix-dovecot_test/files/default/tests/minitest/default_test.rb
+++ test/cookbooks/postfix-dovecot_test/files/default/tests/minitest/default_test.rb
@@ -23,21 +23,11 @@ describe 'postfix-dovecot::default' do
   include Helpers::PostfixDovecotTest
 
   describe 'postfix' do
-    it 'postfix should be running' do
-      service('postfix').must_be_running
-    end
-
     it 'should be able to send mails through SES' do
       skip unless node['postfix-dovecot']['ses']['enabled']
       send_test_mail(
         node['postfix-dovecot']['ses']['email'], 'blackhole@zuazo.org'
       )
-    end
-  end
-
-  describe 'dovecot' do
-    it 'dovecot should be running' do
-      service('dovecot').must_be_running
     end
   end
 end
