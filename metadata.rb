3ee4f240b5697901942a5a348419a20cde7634a2
Xabier de Zuazo <xabier@zuazo.org>
2015-09-03 03:14:04 +0200

    version bumped to 2.0.1 WiP

--- metadata.rb
+++ metadata.rb
@@ -25,7 +25,7 @@ license 'Apache 2.0'
 description 'Installs and configures a mail server using Postfix, Dovecot, '\
             'PostfixAdmin and SpamAssassin, including Amazon SES support.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '2.0.0'
+version '2.0.1' # WiP
 
 if respond_to?(:source_url)
   source_url "https://github.com/zuazo/#{name}-cookbook"
